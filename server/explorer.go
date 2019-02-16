09f15adab8992dfeea16a4a36332a673a62ccfe4
Ashish Nigam <ashishnigam04@gmail.com>
2019-02-16 14:15:24 +0530

    1) Added implementation of two more methods getCandidates and getBucketsByCandidate based on protobuf 2) Corrected existing implementation after change of candidates -> delegates

--- server/explorer.go
+++ server/explorer.go
@@ -1,7 +1,10 @@
 package server
 
 import (
+	"encoding/hex"
+	"errors"
 	"strconv"
+	"time"
 
 	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/iotexproject/iotex-election/committee"
@@ -23,18 +26,67 @@ func (s *server) GetMeta(ctx context.Context, empty empty.Empty) (*pb.ChainMeta,
 	if err != nil {
 		return &pb.ChainMeta{}, err
 	}
+
 	return &pb.ChainMeta{
 		Height:          strconv.FormatUint(height, 10),
-		TotalCandidates: int64(len(result.Candidates())),
+		TotalCandidates: uint64(len(result.Delegates())),
 	}, nil
 }
 
 // GetCandidates returns a list of candidates sorted by weighted votes
-func (s *server) GetCandidates(ctx context.Context, cr *pb.GetCandidatesRequest) (*pb.CandidateResponse, error) {
-	return new(pb.CandidateResponse), nil
+func (s *server) GetCandidates(ctx context.Context, request *pb.GetCandidatesRequest) ([]*pb.CandidateResponse, error) {
+	offset := request.Offset
+	limit := request.Limit
+	if offset < 0 || limit < 0 {
+		return nil, errors.New("offset and limit should be positive number")
+	}
+	height, err := strconv.ParseUint(request.Height, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	result, err := s.c.ResultByHeight(height)
+	if err != nil {
+		return nil, err
+	}
+	candidates := result.Delegates()
+	if uint64(len(candidates)) <= offset {
+		return nil, errors.New("offset is larger than candidate length")
+	}
+	if uint64(len(candidates)) < offset+limit {
+		limit = uint64(len(candidates)) - offset
+	}
+	retval := make([]*pb.CandidateResponse, limit)
+	for i := offset; i < offset+limit; i++ {
+		candidate := candidates[i]
+		retval[i].Candidates[i].Name = hex.EncodeToString(candidate.Name())
+		retval[i].Candidates[i].PubKey = hex.EncodeToString(candidate.BeaconPubKey())
+		retval[i].Candidates[i].TotalWeightedVotes = candidate.Score().Text(10)
+	}
+
+	return retval, nil
 }
 
 // GetBucketsByCandidate returns the buckets
-func (s *server) GetBucketsByCandidate(ctx context.Context, br *pb.GetBucketsByCandidateRequest) (*pb.BucketResponse, error) {
-	return new(pb.BucketResponse), nil
+func (s *server) GetBucketsByCandidate(ctx context.Context, request *pb.GetBucketsByCandidateRequest) ([]*pb.BucketResponse, error) {
+	height, err := strconv.ParseUint(request.Height, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	result, err := s.c.ResultByHeight(height)
+	if err != nil {
+		return nil, err
+	}
+	votes := result.VotesByDelegate([]byte(request.Name))
+	if votes == nil {
+		return nil, errors.New("No buckets for the candidate")
+	}
+	retval := make([]*pb.BucketResponse, len(votes))
+	for i, vote := range votes {
+		retval[i].Buckets[i].Voter = hex.EncodeToString(vote.Voter())
+		retval[i].Buckets[i].Votes = vote.Amount().Text(10)
+		retval[i].Buckets[i].WeightedVotes = vote.WeightedAmount().Text(10)
+		retval[i].Buckets[i].RemainingDuration = uint64(vote.RemainingTime(result.MintTime()) / time.Second)
+	}
+
+	return retval, nil
 }

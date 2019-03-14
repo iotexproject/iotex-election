5549ec9a426ab7f0836c5f886a7d3703fe775ff8
Tian Pan <puncsky@gmail.com>
2019-03-14 13:36:40 -0700

    return SelfStakingTokens

--- server/ranking/ranking.go
+++ server/ranking/ranking.go
@@ -139,10 +139,12 @@ func (s *server) GetCandidates(ctx context.Context, request *pb.GetCandidatesReq
 	}
 	for i := uint32(0); i < limit; i++ {
 		candidate := candidates[offset+i]
+		candidate.SelfStakingTokens()
 		response.Candidates[i] = &pb.Candidate{
 			Name:               hex.EncodeToString(candidate.Name()),
 			Address:            hex.EncodeToString(candidate.Address()),
 			TotalWeightedVotes: candidate.Score().Text(10),
+			SelfStakingTokens:  candidate.SelfStakingTokens().Text(10),
 		}
 	}
 

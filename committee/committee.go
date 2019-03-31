c9872b60357acc45dee898f381eceeace8b4a3f3
lzxm160 <lzxm160@sina.com>
2019-03-31 18:11:59 +0800

    add test vote and candidate filter

--- committee/committee.go
+++ committee/committee.go
@@ -8,7 +8,6 @@ package committee
 
 import (
 	"context"
-	"fmt"
 	"math"
 	"math/big"
 	"sort"
@@ -356,7 +355,6 @@ func (ec *committee) voteFilter(v *types.Vote) bool {
 	return ec.voteThreshold.Cmp(v.Amount()) > 0
 }
 func (ec *committee) candidateFilter(c *types.Candidate) bool {
-	fmt.Println(ec.scoreThreshold.Text(10), ":", c.Score().Text(10))
 	return ec.selfStakingThreshold.Cmp(c.SelfStakingTokens()) > 0 ||
 		ec.scoreThreshold.Cmp(c.Score()) > 0
 }

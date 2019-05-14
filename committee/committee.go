21803576fafc2f320288dc20cd991fa6b6c825ab
lzxm160 <lzxm160@sina.com>
2019-05-14 15:07:04 +0800

    fix comments

--- committee/committee.go
+++ committee/committee.go
@@ -2,7 +2,7 @@
 // This program is free software: you can redistribute it and/or modify it under the terms of the
 // GNU General Public License as published by the Free Software Foundation, either version 3 of
 // the License, or (at your option) any later version.
-// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 // without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
 // the GNU General Public License for more details.
 // You should have received a copy of the GNU General Public License along with this program. If
@@ -261,9 +261,12 @@ func (ec *committee) fetchInBatchSplit(tipHeight uint64) error {
 			ec.currentHeight = tipHeight
 		}
 		results, errs := ec.fetchInBatch(ec.currentHeight)
+		ec.mutex.Lock()
 		if err := ec.storeInBatch(results, errs); err != nil {
+			ec.mutex.Unlock()
 			return errors.Wrap(err, "failed to catch up via network")
 		}
+		ec.mutex.Unlock()
 	}
 	return nil
 }
@@ -302,8 +305,6 @@ func (ec *committee) storeInBatch(
 	results map[uint64]*types.ElectionResult,
 	errs map[uint64]error,
 ) error {
-	ec.mutex.Lock()
-	defer ec.mutex.Unlock()
 	var heights []uint64
 	for height := range results {
 		heights = append(heights, height)

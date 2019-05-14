e9e6383dcd6547bf6c4d38be0b710eb40e884e78
CoderZhi <thecoderzhi@gmail.com>
2019-05-13 17:22:27 -0700

    export reward and operator address (#61)

--- util/util.go
+++ util/util.go
@@ -2,7 +2,7 @@
 // This program is free software: you can redistribute it and/or modify it under the terms of the
 // GNU General Public License as published by the Free Software Foundation, either version 3 of
 // the License, or (at your option) any later version.
-// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 // without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
 // the GNU General Public License for more details.
 // You should have received a copy of the GNU General Public License along with this program. If
@@ -30,4 +30,14 @@ func CopyBytes(b []byte) []byte {
 	copy(c, b)
 
 	return c
+}
+
+// IsAllZeros return true if all bytes are zero
+func IsAllZeros(b []byte) bool {
+	for _, v := range b {
+		if v != 0 {
+			return false
+		}
+	}
+	return true
 }

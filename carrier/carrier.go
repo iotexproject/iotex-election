c4add357657d251a1ff6e4333a7b8b756faa06db
lzxm160 <lzxm160@sina.com>
2019-05-17 16:25:32 +0800

    Downgrade the logs of showing the invalid operator/reward address. #64

--- carrier/carrier.go
+++ carrier/carrier.go
@@ -2,7 +2,7 @@
 // This program is free software: you can redistribute it and/or modify it under the terms of the
 // GNU General Public License as published by the Free Software Foundation, either version 3 of
 // the License, or (at your option) any later version.
-// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 // without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
 // the GNU General Public License for more details.
 // You should have received a copy of the GNU General Public License along with this program. If
@@ -96,7 +96,7 @@ func (evc *ethereumCarrier) BlockTimestamp(height uint64) (ts time.Time, err err
 			context.Background(),
 			big.NewInt(0).SetUint64(height),
 		); err == nil {
-			ts = time.Unix(int64(header.Time), 0)
+			ts = time.Unix(header.Time.Int64(), 0)
 			return
 		}
 		var rotated bool

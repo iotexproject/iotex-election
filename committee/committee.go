54de7f8a3ee0d8655ccb9c2aea209dffecaa253d
lzxm160 <lzxm160@sina.com>
2019-05-21 08:15:28 +0800

    fix comments

--- committee/committee.go
+++ committee/committee.go
@@ -2,7 +2,7 @@
 // This program is free software: you can redistribute it and/or modify it under the terms of the
 // GNU General Public License as published by the Free Software Foundation, either version 3 of
 // the License, or (at your option) any later version.
-// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 // without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
 // the GNU General Public License for more details.
 // You should have received a copy of the GNU General Public License along with this program. If
@@ -202,7 +202,7 @@ func (ec *committee) Start(ctx context.Context) (err error) {
 				ec.terminate <- true
 				return
 			case height := <-heightChan:
-				if err := ec.SyncInSplit(height); err != nil {
+				if err := ec.SyncInBatch(height); err != nil {
 					zap.L().Error("failed to sync", zap.Error(err))
 				}
 			case err := <-reportChan:
@@ -235,33 +235,32 @@ func (ec *committee) Status() STATUS {
 	}
 }
 
-func (ec *committee) SyncInSplit(tipHeight uint64) error {
+func (ec *committee) SyncInBatch(tipHeight uint64) error {
 	zap.L().Info("new ethereum block", zap.Uint64("height", tipHeight))
 	fetchStart := ec.nextHeight + ec.interval
 	if tipHeight < fetchStart {
 		return nil
 	}
 	fetchHeight := uint64(ec.fetchInParallel) * ec.interval
+	sync := func(tipHeight uint64) error {
+		results, errs := ec.fetchInBatch(tipHeight)
+		ec.mutex.Lock()
+		defer ec.mutex.Unlock()
+
+		return ec.storeInBatch(results, errs)
+	}
 	for ec.currentHeight < tipHeight {
 		if ec.nextHeight+fetchHeight < tipHeight {
 			ec.currentHeight = ec.nextHeight + fetchHeight
 		} else {
 			ec.currentHeight = tipHeight
 		}
-		err := ec.Sync(ec.currentHeight)
+		err := sync(ec.currentHeight)
 		if err != nil {
 			return err
 		}
 	}
 	return nil
-}
-
-func (ec *committee) Sync(tipHeight uint64) error {
-	results, errs := ec.fetchInBatch(tipHeight)
-	ec.mutex.Lock()
-	defer ec.mutex.Unlock()
-
-	return ec.storeInBatch(results, errs)
 }
 
 func (ec *committee) fetchInBatch(tipHeight uint64) (

19a4dbe7c598d3fbc8e1033a1207156371485b1b
Yutong Pei <yutong@iotex.io>
2019-05-30 12:01:32 -0700

    update

--- votesync/votesync.go
+++ votesync/votesync.go
@@ -5,6 +5,7 @@ import (
 	"encoding/hex"
 	"log"
 	"math/big"
+	"time"
 
 	"github.com/cenkalti/backoff"
 	"github.com/ethereum/go-ethereum/common"
@@ -17,17 +18,23 @@ import (
 type VoteSync struct {
 	carrier        carrier.Carrier
 	lastHeight     uint64
-	heightInternal uint64
+	lastTimestamp  time.Time
+	timeInternal   time.Duration
 	paginationSize uint8
 	terminate      chan bool
 }
 
 type Config struct {
-	GravityChainAPIs           []string `yaml:"gravityChainAPIs"`
-	GravityChainHeightInterval uint64   `yaml:"gravityChainHeightInterval"`
-	RegisterContractAddress    string   `yaml:"registerContractAddress"`
-	StakingContractAddress     string   `yaml:"stakingContractAddress"`
-	PaginationSize             uint8    `yaml:"paginationSize"`
+	GravityChainAPIs         []string      `yaml:"gravityChainAPIs"`
+	GravityChainTimeInterval time.Duration `yaml:"gravityChainTimeInterval"`
+	RegisterContractAddress  string        `yaml:"registerContractAddress"`
+	StakingContractAddress   string        `yaml:"stakingContractAddress"`
+	PaginationSize           uint8         `yaml:"paginationSize"`
+}
+
+type WeightedVote struct {
+	Voter []byte
+	Votes *big.Int
 }
 
 func NewVoteSync(cfg Config) (*VoteSync, error) {
@@ -42,11 +49,16 @@ func NewVoteSync(cfg Config) (*VoteSync, error) {
 
 	// TODO get lastHeight from iotex contract
 	lastHeight := uint64(7840000)
+	lastTimestamp, err := carrier.BlockTimestamp(lastHeight)
+	if err != nil {
+		return nil, err
+	}
 	return &VoteSync{
 		carrier:        carrier,
-		heightInternal: cfg.GravityChainHeightInterval,
+		timeInternal:   cfg.GravityChainTimeInterval,
 		paginationSize: cfg.PaginationSize,
 		lastHeight:     lastHeight,
+		lastTimestamp:  lastTimestamp,
 		terminate:      make(chan bool),
 	}, nil
 }
@@ -61,8 +73,13 @@ func (vc *VoteSync) Start(ctx context.Context) {
 				vc.terminate <- true
 				return
 			case height := <-heightChan:
-				if vc.heightInternal+vc.lastHeight <= height {
-					if err := vc.sync(vc.lastHeight, height); err != nil {
+				t, err := vc.carrier.BlockTimestamp(height)
+				if err != nil {
+					zap.L().Error("failed to get eth block time stamp", zap.Error(err))
+					continue
+				}
+				if t.After(vc.lastTimestamp.Add(vc.timeInternal)) {
+					if err := vc.sync(vc.lastHeight, height, t); err != nil {
 						zap.L().Error("failed to sync votes", zap.Error(err))
 					}
 				}
@@ -80,38 +97,61 @@ func (vc *VoteSync) Stop(ctx context.Context) {
 	return
 }
 
-func (vc *VoteSync) sync(prevHeight, currHeight uint64) error {
-	a, b, err := vc.fetchVotesUpdate(prevHeight, currHeight)
+func (vc *VoteSync) sync(prevHeight, currHeight uint64, currTs time.Time) error {
+	ret, err := vc.fetchVotesUpdate(prevHeight, currHeight, currTs)
 	if err != nil {
 		return err
 	}
 
 	// TODO write results to iotex contract
-	log.Println(a, b)
+	log.Println(ret)
 	vc.lastHeight = currHeight
+	vc.lastTimestamp = currTs
 	return nil
 }
 
-func (vc *VoteSync) fetchVotesUpdate(prevHeight, currHeight uint64) ([]*types.Vote, [][]byte, error) {
+func (vc *VoteSync) fetchVotesUpdate(prevHeight, currHeight uint64, currTs time.Time) ([]*WeightedVote, error) {
 	prev, err := vc.retryFetchResultByHeight(prevHeight)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 	curr, err := vc.retryFetchResultByHeight(currHeight)
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
-	m := make(map[string]interface{})
-	var cleaned [][]byte
+
+	r := make(map[string]*WeightedVote)
+	for _, v := range curr {
+		vs := types.CalcWeightedVotes(v, currTs)
+		wv, ok := r[hex.EncodeToString(v.Voter())]
+		if ok {
+			wv.Votes.Add(wv.Votes, vs)
+			continue
+		}
+		r[hex.EncodeToString(v.Voter())] = &WeightedVote{
+			Voter: v.Voter(),
+			Votes: vs,
+		}
+	}
+
+	var ret []*WeightedVote
+	for _, v := range r {
+		ret = append(ret, v)
+	}
+
+	m := make(map[string]*types.Vote)
 	for _, v := range prev {
 		m[hex.EncodeToString(v.Voter())] = v
 	}
-	for _, v := range curr {
-		if _, ok := m[hex.EncodeToString(v.Voter())]; !ok {
-			cleaned = append(cleaned, v.Voter())
+	for k, v := range m {
+		if _, ok := r[k]; !ok {
+			ret = append(ret, &WeightedVote{
+				Voter: v.Voter(),
+				Votes: big.NewInt(0),
+			})
 		}
 	}
-	return curr, cleaned, nil
+	return ret, nil
 }
 
 func (vc *VoteSync) retryFetchResultByHeight(h uint64) ([]*types.Vote, error) {

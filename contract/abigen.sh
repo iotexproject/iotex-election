9ecfd1914ca2bd51b2f3619dda29318d9ad44dc4
zhi <thecoderzhi@gmail.com>
2019-06-11 13:36:51 -0700

    update abi and contract go files

--- contract/abigen.sh
+++ contract/abigen.sh
@@ -2,8 +2,8 @@
 
 abigen --abi iotx.abi --pkg contract --type IOTX --out iotx.go
 abigen --abi rotatablevps.abi --pkg contract --type RotatableVPS --out rotatablevps.go
-abigen --abi broker.abi --pkg contract --type Broker --out broker.go
-abigen --abi clerk.abi --pkg contract --type Clerk --out clerk.go
-abigen --abi vita.abi --pkg contract --type Vita --out vita.go
+abigen --abi broker.abi --bin broker.bin --pkg contract --type Broker --out broker.go
+abigen --abi clerk.abi --bin clerk.bin --pkg contract --type Clerk --out clerk.go
+abigen --abi vita.abi --bin vita.bin --pkg contract --type Vita --out vita.go
 abigen --abi register.abi --pkg contract --type Register --out register.go
 abigen --abi staking.abi --pkg contract --type Staking --out staking.go

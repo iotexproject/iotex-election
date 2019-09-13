14998c9b879a2b2730b036009ad6820712167652
koseoyoung <dorothy0602@gmail.com>
2019-09-12 17:41:02 -0700

    change data type

--- db/tablestore.go
+++ db/tablestore.go
@@ -64,7 +64,7 @@ func (t *tableDB) InitializeTable () error {
 
 //NewBucketTable creates buckets table
 func (t *tableDB) NewBucketTable() error {
-	_, err := t.db.Exec("CREATE TABLE IF NOT EXISTS buckets (id INTEGER PRIMARY KEY AUTOINCREMENT, hash BLOB, startTime BLOB, duration TEXT, amount BLOB, decay INTEGER, voter BLOB, candidate BLOB, bucketIndex INTEGER)")
+	_, err := t.db.Exec("CREATE TABLE IF NOT EXISTS buckets (id INTEGER PRIMARY KEY AUTOINCREMENT, hash BLOB, startTime BLOB, duration TEXT, amount BLOB, decay INTEGER, voter BLOB, candidate BLOB, bucketIndex BLOB)")
 	return err
 }
 //NewRegistrationTable creates registrations table 
@@ -74,17 +74,17 @@ func (t *tableDB) NewRegistrationTable() error {
 }
 //NewHeightToRegTable creates heightToReg table
 func (t *tableDB) NewHeightToRegTable() error {
-	_, err := t.db.Exec("CREATE TABLE IF NOT EXISTS heightToReg (height INTEGER, index INTEGER)")
+	_, err := t.db.Exec("CREATE TABLE IF NOT EXISTS heightToReg (height BLOB, index INTEGER)")
 	return err
 }
 //NewHeightToBucketTable creates heightToBucket table
 func (t *tableDB) NewHeightToBucketTable() error {
-	_, err := t.db.Exec("CREATE TABLE IF NOT EXISTS heightToBucket (height INTEGER, index INTEGER)")
+	_, err := t.db.Exec("CREATE TABLE IF NOT EXISTS heightToBucket (height BLOB, index INTEGER)")
 	return err
 }
 //NewHeightToTimeTable creates heightToTime table
 func (t *tableDB) NewHeightToTimeTable() error {
-	_, err := t.db.Exec("CREATE TABLE IF NOT EXISTS heightToTime (height INTEGER, time BLOB)")
+	_, err := t.db.Exec("CREATE TABLE IF NOT EXISTS heightToTime (height BLOB, time BLOB)")
 	return err
 }
 
